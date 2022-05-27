5852e248df6fb7af5664652553823d7a63ba30ba
Inna Akopova <akopova.inna@gmail.com>
2022-05-27 19:18:34 +0300

    init

---
+++ view/header.php
@@ -0,0 +1,41 @@
+<header>    	
+		<div class="info">
+			<a href="main.html" >
+			<img src="logo.png" class="logo" alt="Heading image">
+		</a>
+		<h2>Latvijas trifeļu entuziasti</h2>
+			</div>
+		<div class="nav">
+		<button class="button2" onclick="openNav()">&#9776; Menu</button>
+		</div>
+	
+   <div id="myNav" class="overlay">
+
+	<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  
+	<div class="overlay-content"> 
+	  <a href="main.html">Galvenā</a>
+	  <a href="trifeles.html">Kas ir trifeles?</a>
+	  <a href="#numlist">Trifeļu īpašības</a>
+	  <a href="trifeles2.html">Fakti par trifelem</a>
+	</div>
+
+	</div>
+  
+
+<script>
+	function openNav() {
+		document.getElementById("myNav").style.width = "40%";
+	  }
+	  
+	  function closeNav() {
+		document.getElementById("myNav").style.width = "0%";
+	  }
+</script>
+
+  <div class="maintitle">
+		<h1><strong>Uzsāc vērtigo trifeļu audzēšanu savā saimniecībā!</strong></h1> 
+  </div>
+
+		
+	</header>
